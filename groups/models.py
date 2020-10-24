ed849f92b1fc0a52dff1c9470f0dbfec2d2b7786
Julian <rosasjulian484@gmail.com>
2020-10-24 12:43:44 -0500

    pagina con flujo funcional

--- groups/models.py
+++ groups/models.py
@@ -17,20 +17,7 @@ class Group(models.Model):
     slug = models.SlugField(allow_unicode = True, unique = True)
     description = models.TextField(blank=True, default="")
     description_html= models.TextField(editable=False, default="",blank= True)
-
-
-class GroupMember(models.Model):
-    group = models.ForeignKey(Group,related_name='membership',on_delete=models.CASCADE)
-    user = models.ForeignKey(User, related_name='user_groups',on_delete=models.CASCADE)
-
-    def __str__(self):
-        return self.user.username
     
-    class Meta:
-        unique_together = ('group','user')
-    
-
-
     def __str__(self):
         return self.name
     
@@ -44,3 +31,14 @@ class GroupMember(models.Model):
     
     class Meta:
         ordering = ["name"]
+
+
+class GroupMember(models.Model):
+    group = models.ForeignKey(Group,related_name='membership',on_delete=models.CASCADE)
+    user = models.ForeignKey(User, related_name='user_groups',on_delete=models.CASCADE)
+
+    def __str__(self):
+        return self.user.username
+    
+    class Meta:
+        unique_together = ('group','user')
